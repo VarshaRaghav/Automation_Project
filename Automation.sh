8b5100c3d223eee4e6796caea6be25dddbcf5045
VarshaRaghav <varsha.diwakar6692@gmail.com>
2022-12-14 21:25:30 +0530

    Third Task Updated

--- Automation.sh
+++ Automation.sh
@@ -1,6 +1,12 @@
 myname="Varsha"
 s3_bucket="upgrad-varshad"
 timestamp=$(date '+%d%m%Y-%H%M%S')
+pathname=/tmp/
+logname=${myname}-httpd-logs-${timestamp}.tar
+size=$(du -h ${file} | awk '{print $1}')
+sitepath="/var/www/html"
+cronpath=/etc/cron.d/Automation
+file=${pathname}${logname}
 sudo apt update -y
 sudo apt install apache2
 sudo ufw app list
@@ -11,3 +17,23 @@ sudo apt install awscli
 aws configure
 Default output format [None]:
 aws s3 cp --recursive /tmp/${myname}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
+
+
+if test -f "${sitepath}/inventory.html"; then
+    echo "inventory exists";
+else
+    echo "creating inventory";
+    echo -e 'Log Type\t-\tTime Created\t-\tType\t-\tSize' > ${sitepath}/inventory.html
+fi
+
+if test -f "${sitepath}/inventory.html"; then
+    echo "updating inventory";
+    size=$(du -h ${file} | awk '{print $1}')
+        echo -e "/tmp/${name}-httpd-logs\t-\t${timestamp}\t-\ttar\t-\t${size}" >> ${sitepath}/inventory.html
+fi
+
+if test -f ${cronpath}; then
+    echo "cron job exists";
+else
+echo "     * root/Automation_Project/automation.sh" >> ${cronpath}
+fi
